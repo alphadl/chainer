eb22d2f8a1f22134564f286ffa45f487dbfacfaf
Keisuke Umezawa <keisuke.umezawa@gmail.com>
2017-03-13 21:28:11 +0900

    follow code reviews

--- chainer/functions/activation/hard_sigmoid.py
+++ chainer/functions/activation/hard_sigmoid.py
@@ -69,11 +69,11 @@ def hard_sigmoid(x):
 
     .. admonition:: Example
 
-        >>> x = np.arange(-2, 3, 2).astype('f')
+        >>> x = np.array([-2.6, -1, 0, 1, 2.6])
         >>> x
-        array([-2.,  0.,  2.], dtype=float32)
+        array([-2.6, -1. ,  0. ,  1. ,  2.6])
         >>> F.hard_sigmoid(x).data
-        array([ 0.09999999,  0.5       ,  0.89999998], dtype=float32)
+        array([ 0. ,  0.3,  0.5,  0.7,  1. ])
 
     """
     return HardSigmoid()(x)
